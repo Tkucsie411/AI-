2ff08eed6d249e8de9402eaa5e0aba51c9681c97
tkucsie411 <tkucsie411@gmail.com>
2025-04-06 14:09:44 +0800

    app更新跳轉page3

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import openai
+import os
+from dotenv import load_dotenv
+
+#載入.env檔案中的環境變數
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app) #允許前端請求後端API
+
+#設定openAI API Key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #建立網站首頁
 @app.route('/')
@@ -14,6 +25,10 @@ def page1():
 @app.route('/page2')
 def page2():
     return render_template('page2.html')
+
+@app.route('/page3')
+def page3():
+    return render_template('page3.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='127.0.0.1', port=5000)
